5b2372ee7db0cbe573d2981e7c15204126bae214
teohaik <teohaik@gmail.com>
2021-11-03 19:40:44 +0200

    init code

---
+++ src/main/java/movieclub/Main.java
@@ -0,0 +1,30 @@
+package movieclub;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Movie Twister = new Movie("Twister", Movie.REGULAR);
+		Movie Mickey = new Movie("Mickey", Movie.CHILDRENS);
+		
+		Rental aRental = new Rental(Twister, 3);
+		Rental bRental = new Rental(Mickey, 5);
+		
+		Customer Bob = new Customer("Bob");
+		Bob.addRental(aRental);
+		Bob.addRental(bRental);
+		
+		System.out.println(Bob.statement());
+	}
+
+
+	public void problematicMethod(){
+		int z= 0;
+		if(System.currentTimeMillis()< 50){
+			z = 5;
+		}
+
+		int test = 150 / z;
+		
+	}
+
+}
